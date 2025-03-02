1dee6433ddfc8a177b8f214824304bc5df98a72f
mvtvn78 <mvtvn78@gmail.com>
2025-03-02 16:22:42 +0700

    update

--- face_reg/vd.py
+++ face_reg/vd.py
@@ -1,4 +1,5 @@
 import cv2
+import random
 
 face_detect = cv2.CascadeClassifier("models\haarcascade_frontalface_default.xml")
 video = cv2.VideoCapture(0)
@@ -6,7 +7,9 @@ while True:
     _, frame = video.read()
     faces = face_detect.detectMultiScale(frame, 1.3, 5)
     for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y, w, h), (0, 0, 255), 2)
+        cv2.rectangle(frame, (x, y, w, h), (0, 0, 255), 1)
+        detect_img = frame[y : y + h - 2, x : x + w - 2]
+        cv2.imwrite(f"testing{random.randrange(1, 10, 1)}.png", detect_img)
     cv2.imshow("frame", frame)
     if cv2.waitKey(30) & 0xFF == ord("q"):
         break
